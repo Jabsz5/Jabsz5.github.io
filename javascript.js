61ac312f618c17804da23214d2cd6b15879b0ba2
Jabsz5 <142645307+Jabsz5@users.noreply.github.com>
2024-03-30 17:06:37 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,21 @@
+let myenglishText = `<p>This is some text in English. This serves to prove my ability to write in English.
+Although I am good at typing my native language,
+I do have some trouble speaking it :p<p>`;
+
+let myspanishText = `Esto es texto en español. 
+Este es el segundo idioma que hablo yo y el idioma de mis papás. 
+Espero un día llegar a un nivel como el del mi inglés`;
+
+let myfrenchText = `C'est le texte en français ! C'est le troisième langage que je parle. 
+C'est très similaire à l'espagnol donc c'etait un peu facil d'apprendre... :p`;
+
+let aboutInfo = `I'm not sure what I want to exactly put in this website :p
+So for now, I'm just going to make it purrty and put some stuff about me`;
+
+document.getElementById("englishText").innerHTML = myenglishText;
+
+document.getElementById("spanishText").innerHTML = myspanishText;
+
+document.getElementById("frenchText").innerHTML = myfrenchText;
+
+document.getElementById("about").innerHTML = aboutInfo;
